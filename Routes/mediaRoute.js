4e9fbc8a446fd08744c59d89a30acf3871b7dc3f
Eric Gomez <eric_gomez_1@hotmail.com>
2019-01-17 18:47:39 -0800

    migrated the media storage from the heroku server to AWS

--- Routes/mediaRoute.js
+++ Routes/mediaRoute.js
@@ -1,8 +1,15 @@
+require('dotenv').config()
 const router = require('express').Router();
 const Note = require('../Models/Note');
 const fs = require('fs');
 const uuidv4 = require('uuid/v4');
 const MEDIA = `${__dirname}/../Media`;
+const aws = require('aws-sdk');
+aws.config.update({
+  accessKeyId: process.env.AWSAccessKeyId,
+  secretAccessKey: process.env.AWSSecretKey,
+});
+const s3 = new aws.S3();
 
 const decodeBase64Image = (dataString) => {
   const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
@@ -18,14 +25,19 @@ const decodeBase64Image = (dataString) => {
 router.route('/images/:image')
   .get((req, res) => {
     const { image, id } = req.params;
-    fs.readFile(`${MEDIA}/Images/${image}`, (err, file) => {
+    const params = {
+      Bucket: 'notesphotostorage',
+      Key: image,
+    }
+    s3.getObject(params, (err, image) => {
       if (err) {
         res.status(404).json({'error': 'no image found'});
       } else {
-        res.set('Content-Type', 'image/jpg').send(file);
+        res.set('Content-Type', 'image/jpg').send(image.Body);
       }
-    })
+    });
   })
+
 router.route('/images/:id')
   .put((req, res) => {
     const { id } = req.params;
@@ -33,22 +45,34 @@ router.route('/images/:id')
     image = decodeBase64Image(image);
     const EXT = image.type.match(/image\/(.+)/)[1];
     const NAME = `${uuidv4()}.${EXT}`;
-    fs.writeFile(`${MEDIA}/Images/${NAME}`, image.data, function(err) {
-      if (err) console.log(err);
-    });
-    Note.findById(id, (err, note) => {
-      if (err) console.log(err);
-      if (note.image.name !== '') {
-        fs.unlink(`${MEDIA}/Images/${note.image.name}`, (err) => {
-          if (err) console.log(err);
-        });
+    const params = {
+      Body: image.data,
+      Bucket: 'notesphotostorage',
+      Key: NAME
+    }
+    s3.putObject(params, (err, data) => {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        Note.findById(id, (err, note) => {
+          if (err) console.log("Error", err);
+          if (note.image !== '') {
+            const params = {
+              Bucket: "notesphotostorage",
+              Key: note.image.name
+            };
+            s3.deleteObject(params, function(err, data) {
+              if (err) console.log("Delete Object ERROR", err);
+            });
+          }
+          note.image = {
+            "name": NAME,
+            "type": EXT,
+          };
+          note.save();
+          return res.status(200).json(note);
+        })
       }
-      note.image = {
-        "name": NAME,
-        "type": EXT,
-      };
-      note.save();
-      return res.status(200).json(note);
     })
   })
   .delete((req, res) => {
@@ -56,12 +80,15 @@ router.route('/images/:id')
     Note.findById(id, (err, note) => {
       if(err) return res.status(500).json(err);
       if (note.image.name !== '') {
-        fs.unlink(`${MEDIA}/Images/${note.image.name}`, (err) => {
-          if (err) console.log("File not found.");
-          note.image = '';
-          note.save();
-          res.status(200).json(note);
-        });
+        const params = {Bucket: 'notesphotostorage', Key: note.image.name};
+        s3.deleteObject(params, (err, data) => {
+          if (err) console.log(err);
+          else {
+            note.image = '';
+            note.save();
+            res.json(note);
+          }
+        })
       }
     })
   })
